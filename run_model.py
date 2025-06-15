0ec8c26dcd47aad8b3f352f52e8ecbd48080b3fe
Tamim Dostyar <a.tamimdostyar@gmail.com>
2025-06-15 17:27:40 -0500

    added files

--- run_model.py
+++ run_model.py
@@ -1,10 +1,18 @@
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import tensorflow as tf
+import os
 
 def run_model(img_path):
     IMG_SIZE = (224, 224)
-    model = tf.keras.models.load_model("/app/models/my_cancer_cnn_model.h5")
+
+    try:
+        model_path = os.path.join("models", "my_cancer_cnn_model.h5")
+        model = tf.keras.models.load_model(model_path)
+    except Exception as e:
+        print(f"Error loading model: {str(e)}")
+        raise
+    
     class_labels = {
         "lung_aca": "Lung Adenocarcinoma",
         "lung_n": "Lung Benign Tissue (Normal)",
@@ -20,5 +28,6 @@ def run_model(img_path):
     predictions = model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
     predicted_key = list(class_labels.keys())[np.argmax(score)]
+    predicted_label = class_labels[predicted_key]
     confidence = (100 * np.max(score))
-    return predicted_key, confidence
+    return predicted_label, confidence
